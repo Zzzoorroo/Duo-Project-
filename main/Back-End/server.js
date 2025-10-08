a575f214a8791a1d53a9c13418373312bcf75ba4
ILYASS BELMEHDI <99798385+Zzzoorroo@users.noreply.github.com>
2025-10-08 23:30:52 +0800

    working on the username

--- main/Back-End/server.js
+++ main/Back-End/server.js
@@ -25,7 +25,7 @@ io.on(`connection`, (socket) => {
     socket.on(`message`, (data) => {
         console.log(`Received message:`, data);
 
-        io.emit('message', data);
+        io.emit('message', `${socket.id.substr(0, 2)} said ${data}`);
     });
 
     socket.on('disconnect', () => {
