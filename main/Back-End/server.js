f29cfe69e2fafa9974a4b053f8fb54c902a02130
ILYASS BELMEHDI <99798385+Zzzoorroo@users.noreply.github.com>
2025-10-10 22:03:26 +0800

    trying to setup database
    kinda did it

--- main/Back-End/server.js
+++ main/Back-End/server.js
@@ -1,3 +1,13 @@
+//database 
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/chatdb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log(err));
+
+
 
 const express = require("express");
 const http = require("http");
